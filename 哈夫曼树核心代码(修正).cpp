65ce6ab0f3637f1ce126445926f599c9ba0a84c0
王宇航 <1571653321@qq.com>
2021-10-01 21:05:40 +0800

    哈夫曼树的核心代码

---
+++ 哈夫曼树核心代码(修正).cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#define MAXLEAF 30
+#define MAXNODE 2*MAXLEAF-1
+#define MAXBIT 100
+#define MAX 9999
+using namespace std;
+typedef struct {
+	int val;
+	int weight;
+	int parent;
+	int lchild;
+	int rchild;
+}HFnodeType;
+
+typedef struct {
+	int bit[MAXBIT];
+	int start;
+}HFcodeType;
+
+HFnodeType HFnode[MAXNODE];
+HFcodeType HFcode[MAXLEAF];
+
+void HuffmanTree(HFnodeType HFnode[MAXNODE],int n) {
+	int i, x1, x2, j;
+	double m1, m2;
+	cout << "Please input the value and weight of the leaf" <<endl;
+	for(i = 0; i < n; i++) {
+		cin >> HFnode[i].val >> HFnode[i].weight;
+	}
+	for(i = 0; i < 2 * n - 1; i++) {
+		HFnode[i].lchild = -1;
+		HFnode[i].rchild = -1;
+		HFnode[i].parent = -1;
+		HFnode[i].weight = 0;
+	}
+	
+	for(i = 0; i < 2 * n - 1; i++) {
+		x1 = x2 = 0;
+		m1 = m2 = MAX;
+		for(j = 0; j < n + i; j++) {
+			if(HFnode[j].weight < m1 && HFnode[j].parent == -1) {
+				m2 = m1;
+				x2 = x1;
+				m1 = HFnode[j].weight;
+				x1 = j;
+			}
+			else if(HFnode[j].weight < m2 && HFnode[j].parent == -1) {
+				m2 = HFnode[j].weight;
+				x2 = j;
+			}
+		}
+		HFnode[n + i].weight = m1 + m2;
+		HFnode[n + i].rchild = x2;
+		HFnode[n + i].lchild = x1;
+		HFnode[x1].parent = n + i;
+		HFnode[x2].parent = n + i;
+	}
+}
+
+void HuffmanCode(HFcodeType HFcode[MAXLEAF],int n) {
+	HFcodeType cd;
+	int c, p, i, j;
+	for(i = 0; i < n; i++) {
+		cd.start = n - 1;
+		c = i;
+		p = HFnode[c].parent;
+		while(p != NULL) {
+			if(HFnode[p].lchild == c) {
+				cd.bit[cd.start] == 0;
+			}
+			else {
+				cd.bit[cd.start] == 1;
+			}
+			cd.start--;
+			c = p;
+			p = HFnode[c].parent;
+		}
+		for(j = cd.start + 1; j < n; j++) {
+			HFcode[i].bit[j] = cd.bit[j];
+		}
+		HFcode[i].start = cd.start;
+	}
+}
+
+int main()
+{
+	int n;
+	cout << "Please input the number of leaf" <<endl;
+	cin >> n;
+	HuffmanTree(HFnode,n);
+	HuffmanCode(HFcode,n);
+	return 0;
+}
